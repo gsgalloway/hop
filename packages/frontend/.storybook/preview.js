a4a5a652ae21e9d1a235af3e008cd8a9b58b0850
Isaac Kang <isaac.kang@protonmail.ch>
2021-10-06 14:48:17 -0400

    add stylesheet to stories

--- packages/frontend/.storybook/preview.js
+++ packages/frontend/.storybook/preview.js
@@ -3,6 +3,7 @@ import ThemeProvider from 'src/theme/ThemeProvider'
 import Web3Context from 'src/contexts/Web3Context'
 import AppContext from 'src/contexts/AppContext'
 import { MemoryRouter } from 'react-router'
+import 'src/App.css'
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
@@ -17,6 +18,10 @@ export const parameters = {
 export const decorators = [
   Story => (
     <MemoryRouter initialEntries={['/']}>
+      <link
+        href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;300;400;600;700;800;900&display=swap"
+        rel="stylesheet"
+      />
       <ThemeProvider>
         <Web3Context>
           <AppContext>
